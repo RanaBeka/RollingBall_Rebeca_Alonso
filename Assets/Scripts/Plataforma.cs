a574472f0e7947c5293bdaab916dbcde6f3f709c
RanaBeka <217ramram@gmail.com>
2024-09-30 17:09:38 +0200

    descansito

---
+++ Assets/Scripts/Plataforma.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plataforma : MonoBehaviour
+{
+    [SerializeField] Vector3 direccion = new Vector3(0, 0, 0);
+    [SerializeField] int velocidad;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direccion * velocidad * Time.deltaTime);
+        timer += 1 * Time.deltaTime;
+        if (timer >= 5)
+        {
+            direccion = direccion * -1;
+            timer = 0;
+            direccion = direccion * 1;
+        }
+        
+        
+    }
+}
