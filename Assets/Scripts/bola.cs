e46878b74746c84383c59f95fe6e292f376b8032
RanaBeka <217ramram@gmail.com>
2024-10-03 21:23:17 +0200

    wtf

--- Assets/Scripts/bola.cs
+++ Assets/Scripts/bola.cs
@@ -1,13 +1,17 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class bola : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] Vector3 direccion;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -15,8 +19,16 @@ public class bola : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        direccion.x = h;
+        direccion.z = v;
 
-        transform.Translate(new Vector3(h, 0 , 0) * 1 * Time.deltaTime, Space.World);
-        transform.Translate(new Vector3(0, 0, v) * 1 * Time.deltaTime, Space.World);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, 1, 0) * 2, ForceMode.Impulse);
+
+        }
+            rb.AddForce(direccion * 2, ForceMode.Impulse);
+            
+
     }
 }
